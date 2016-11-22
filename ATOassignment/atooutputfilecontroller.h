f726ad7aff11672ba3b5cefe8aeb3e7a9ac5365d
Stephanie Helder <stephdephekt@hotmail/.com>
2016-11-22 15:55:05 +1100

    removed unnecessary code

--- ATOassignment/atooutputfilecontroller.h
+++ ATOassignment/atooutputfilecontroller.h
@@ -15,7 +15,7 @@ using namespace std;
 
 //! Create ATO output file
 
-//! Class creates ATO output file in directory chosen by user
+//! Class creates ATO output file as directed by user
 
 
 class AtoOutputFileController
@@ -23,12 +23,10 @@ class AtoOutputFileController
 public:
     AtoOutputFileController();
     QFile *AtoOutput();
-    QDir *UserGetOutputDir();
     void setParentPtr(QWidget *parent);
 
 private:
     QFile file;
-    ifstream *outputdirptr;
     QWidget *parentptr;
 
 };
